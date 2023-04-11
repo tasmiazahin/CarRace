dd38d9259fb7c280e6b4210b9028272aaf183205
Tasmia Zahin <tasmiamaithily@gmail.com>
2023-04-11 22:43:41 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRace
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Speed { get; set; }
+        public double DistanceLeft { get; set; }
+        public double ElapseTime { get; set; }
+        public double Penalty { get; set; }
+        public bool Finished { get; set; }
+        public double TimeToFinish { get; set; }
+
+        public Car()
+        {
+            //default values
+            DistanceLeft = 10000; //meter
+            Speed = 120; //km/h
+            ElapseTime = 0;
+            Penalty = 0;
+            Finished = false;
+        }
+
+        public async static Task<Car> StartCompetation(Car car)
+        {
+            if (car.Finished == false)
+            {
+                //Calculation time to finish 
+                // Time  = Distance /  speed
+                car.TimeToFinish = (car.DistanceLeft / (car.Speed / 3.6));
+                Console.WriteLine($"Name: {car.Name} DistanceLeft: {car.DistanceLeft}");
+
+                //Check if race is finished or going to finish ssoon
+                if (car.DistanceLeft <= 0)
+                {
+                    Console.WriteLine($"{car.Name} : {car.ElapseTime}");
+                    car.Finished = true;
+                }
+                else if (car.TimeToFinish <= 30)
+                {
+                    car.ElapseTime += car.TimeToFinish;
+                    car.DistanceLeft = 0;
+                    Console.WriteLine($"{car.Name} : {car.ElapseTime}");
+                    car.Finished = true;
+                    return car;
+                }
+
+                await EventOccurrence(car);
+                car.ElapseTime += 30;
+                //Calculate distance left after every 30
+                car.DistanceLeft = car.DistanceLeft - ((car.Speed / 3.6) * (30 - car.Penalty));
+
+                // reset penalty after every 30s
+                car.Penalty = 0;
+            }
+            return car;
+        }
+
+        // Make async?
+        public static async Task EventOccurrence(Car car)
+        {
+            // Make a list of evens based on probability to occur
+            //Refuel 1/50
+            //Puncture 2/50
+            //EventOccurrence 5/50
+            //EngineFailure 10/50
+            var events = new List<string> { "Refuel", "Puncture", "Puncture", "WashWindSheild", "WashWindSheild", "WashWindSheild", "WashWindSheild", "WashWindSheild", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure", "EngineFailure" };
+            var rand = new Random();
+            var eventIndex = rand.Next(0, 50);
+
+            if (events.ElementAtOrDefault(eventIndex) != null)
+            {
+                Console.WriteLine($"Event occured in {car.Name}: {events[eventIndex]} <-------");
+
+                if (events[eventIndex] == "Refuel")
+                {
+                    car.Penalty = 30;
+                }
+                else if (events[eventIndex] == "Puncture")
+                {
+                    car.Penalty = 20;
+                }
+                else if (events[eventIndex] == "WashWindSheild")
+                {
+                    car.Penalty = 10;
+                }
+                else if (events[eventIndex] == "EngineFailure")
+                {
+                    car.Speed = car.Speed - 1;
+                }
+            }
+            else
+            {
+                car.Penalty = 0;
+            }
+
+            return;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using CarRace;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRace
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // create cars
+            Car car1 = new Car
+            {
+                Id = 1,
+                Name = "Tesla"
+            };
+
+            Car car2 = new Car
+            {
+                Id = 2,
+                Name = "Mercedes benz"
+            };
+
+            // Check competation status
+            Task t = Task.Run(() => CompetationStatus(new List<Car> { car1, car2 }));
+
+            while (true)
+            {
+                //Start competation
+                var tesla = await Car.StartCompetation(car1);
+                var mercedesbenz = await Car.StartCompetation(car2);
+
+                // Check  if race has completed or not
+                if (tesla.Finished && mercedesbenz.Finished)
+                {
+                    if (tesla.ElapseTime > mercedesbenz.ElapseTime)
+                    {
+                        Console.WriteLine($"Mercedesbenz has won the race with margin  of {Math.Round(tesla.ElapseTime - mercedesbenz.ElapseTime)} seconds");
+                    }
+                    else if (tesla.ElapseTime < mercedesbenz.ElapseTime)
+                    {
+                        Console.WriteLine($"Tesla  own the race with  margin of {Math.Round(mercedesbenz.ElapseTime - tesla.ElapseTime)} seconds");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Racee tied!!!Both car has reached at same time");
+                    }
+                    return;
+                }
+
+                await Wait30Seconds();
+
+            }
+        }
+
+        public async static Task Wait30Seconds(int tick = 2)
+        {
+            Task.Delay(TimeSpan.FromSeconds(tick)).Wait(); //  Force wait for 30 seconds
+        }
+
+        public static async Task CompetationStatus(List<Car> cars)
+        {
+            Console.WriteLine("Press  enter if you want to know competation status");
+            while (true)
+            {
+                DateTime start = DateTime.Now;
+                bool gotKey = false;
+
+                while ((DateTime.Now - start).TotalSeconds < 2)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        gotKey = true;
+                        break;
+                    }
+                }
+
+                if (gotKey)
+                {
+                    Console.ReadKey();
+                    Console.Clear();
+                    cars.ForEach(car =>
+                    {
+                        Console.WriteLine($"Name: {car.Name} DistanceLeft: {car.DistanceLeft}  ElapseTime : {car.ElapseTime} TimeToFinish : {car.TimeToFinish}");
+                    });
+                }
+
+                gotKey = false;
+
+                var isFinishedRace = cars.All(item => item.Finished);
+                if (isFinishedRace)
+                {
+                    return;
+                }
+
+            }
+
+        }
+    }
+}
